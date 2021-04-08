c233c65a12fcb188dfee4846e05b7cbc3ab56255
Stephen Beckett <beckett.stephenj@gmail.com>
2021-04-08 18:25:19 -0400

    Update makeDailyMapsSpain.R
    Updated file with most recent data source link; and updated code to match Spain (rather than Switzerland) and ascertainment bias of 3.

--- COVID19-Event-Risk-Planner/makeDailyMapsSpain.R
+++ COVID19-Event-Risk-Planner/makeDailyMapsSpain.R
@@ -14,54 +14,59 @@ get_token()
 args <- commandArgs(trailingOnly = TRUE)
 current_time <- args[1]
 
-#TODO: finish port
-getDataSpain <- function() {
-  dataurl <- getURL("https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv") # date, abbreviation_canton_and_fl, ncumul_conf
-  data <- read.csv(text = dataurl, stringsAsFactors = FALSE) %>%
-    mutate(date = as_date(date)) %>%
-    arrange(desc(date)) %>%
-    filter(!is.na(ncumul_conf)) %>%
-    select(date = date, code = abbreviation_canton_and_fl, cases = ncumul_conf)
-  geom <<- st_read("map_data/spain-provinces.geojson")
-  pop <- read.csv("map_data/spain_pop.csv", stringsAsFactors = FALSE)
 
-  cur_date <- ymd(gsub("-", "", Sys.Date())) - 1
-  past_date <- ymd(cur_date) - 14
-  data_cur <<- data %>%
-    group_by(code) %>%
-    summarise(code = first(code), cases = first(cases), date = first(date)) %>%
-    as.data.frame()
-  data_past <- data %>%
-    filter(date <= past_date) %>%
-    group_by(code) %>%
-    summarise(code = first(code), cases = first(cases), date = first(date)) %>%
-    as.data.frame()
-  data_join <<- data_cur %>%
-    inner_join(data_past, by = "code", suffix = c("", "_past")) %>%
-    inner_join(pop, by = c("code")) %>%
-    mutate(n = date - date_past) %>%
-    select(-c("name"))
-  pal <<- colorBin("YlOrRd", bins = c(0, 1, 25, 50, 75, 99, 100))
-  legendlabs <<- c("< 1", " 1-25", "25-50", "50-75", "75-99", "> 99", "No or missing data")
+getDataSpain <- function(){
+  spain_geom <<- st_read('map_data/spain-provinces.geojson')
+  #Main COVID-19 hub page: https://cnecovid.isciii.es/covid19/#distribuci%C3%B3n-geogr%C3%A1fica
+  SPAIN<- read.csv("https://cnecovid.isciii.es/covid19/resources/casos_tecnica_provincia.csv", na.strings=FALSE) 
+  #code link file
+  SPAINcode = read.csv("map_data/spain_codenames.csv",encoding="UTF-8",na.strings=FALSE)
+  #Population data comes from  Instituto Nacional de Estadística: https://www.ine.es/jaxiT3/Datos.htm?t=2852#!tabs-tabla
+  
+  DataJoin = c()
+  Counties <- unique(SPAIN$provincia_iso)
+  DataJoin$ProvinceName = Counties
+  for(aa in 1:length(Counties)){
+    Subset = SPAIN[SPAIN$provincia_iso==Counties[aa],] 
+    Dates = as.Date(Subset$fecha)
+    LL = length(Dates)
+    ConfirmedCovidCases = cumsum(Subset$num_casos)
+    #CaseDiff = 10*( Subset$ConfirmedCovidCases[LL] - Subset$ConfirmedCovidCases[LL - 14])/ 14
+    cases = ConfirmedCovidCases[LL]
+    cases_past = ConfirmedCovidCases[LL - 14]
+    n  = ConfirmedCovidCases[LL] - ConfirmedCovidCases[LL - 14]
+    #Make sure difference in cases is positive. If not set to NA.
+    if(n<0){
+      CaseDiff = NA
+    }
+    DataJoin$date[aa] = as.character(Dates[LL])
+    DataJoin$n[aa] = n
+    DataJoin$cases[aa] = cases
+    DataJoin$cases_past[aa] = cases_past
+  }
+  
+  SPAINdata = as.data.frame(DataJoin)
+  SPAINcode = as.data.frame(SPAINcode)
+  spain_data_join <<- inner_join(SPAINdata,SPAINcode,by=c("ProvinceName"="code")) %>% mutate(pop = population2019)
 }
+  
 
 # Create mouse-over labels
-maplabsSwiss <- function(riskData) {
+maplabsSpain <- function(riskData) {
   riskData <- riskData %>%
     mutate(risk = case_when(
-      risk == 100 ~ "> 99",
-      risk == 0 ~ "< 1",
-      is.na(risk) ~ "No data",
+      risk == 100 ~ '> 99',
+      risk == 0 ~ '< 1',
+      is.na(risk) ~ 'No data',
       TRUE ~ as.character(risk)
     ))
   labels <- paste0(
-    "<strong>", paste0("Canton of ", riskData$name), "</strong><br/>",
-    "Current Risk Level: <b>", riskData$risk, ifelse(riskData$risk == "No data", "", " &#37;"), "</b><br/>",
+    "<strong>", riskData$name, "</strong><br/>",
+    "Current Risk Level: <b>",riskData$risk, ifelse(riskData$risk == "No data", "", "&#37;"),"</b><br/>",
     "Latest Update: ", substr(riskData$date, 1, 10)
   ) %>% lapply(htmltools::HTML)
   return(labels)
 }
-
 
 
 # Calculate risk
@@ -74,9 +79,9 @@ calc_risk <- function(I, g, pop) {
 
 ######## Create and save daily map widgets ########
 size <- 50
-asc_bias = 5
+asc_bias = 3
 
-getDataSwiss()
+getDataSpain()
 
 
 data_Nr <- data_join %>%
@@ -89,14 +94,14 @@ riskdt_map <- geom %>% left_join(riskdt, by = c("id" = "code"))
 map <- leaflet() %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   # setView(lat = 37.1, lng = -95.7, zoom = 4) %>%
-  fitBounds(7.5, 47.5, 9, 46) %>%
+  fitBounds(-20, 44, 5, 26) %>%
   addPolygons(
     data = riskdt_map,
     color = "#444444", weight = 0.2, smoothFactor = 0.1,
     opacity = 1.0, fillOpacity = 0.7,
     fillColor = ~ pal(risk),
     highlight = highlightOptions(weight = 1),
-    label = maplabsSwiss(riskdt_map)
+    label = maplabsSpain(riskdt_map)
   ) %>%
   addLegend(
     data = riskdt_map,
@@ -108,6 +113,6 @@ map <- leaflet() %>%
     }
   )
 
-mapshot(map, file = file.path(getwd(), "daily_risk_map_swiss", current_time, paste0(current_time,"_", asc_bias, "_", size, ".png")))
-post_tweet(status = paste0("Swiss canton-level risk estimate update for ",  now("Europe/Zurich"), ".  Estimated risk that at least 1 person is #COVID19 positive for events or other areas where ", size, " individuals are in close contact [Assuming 5:1 ascertainment bias]"),
- media = file.path("daily_risk_map_swiss", current_time, paste0(current_time,"_", asc_bias, "_", size, ".png")))
+mapshot(map, file = file.path(getwd(), "daily_risk_map_spain", current_time, paste0(current_time,"_", asc_bias, "_", size, ".png")))
+post_tweet(status = paste0("Spain province-level risk estimate update for ",  now("Europe/Madrid"), ".  Estimated risk that at least 1 person is #COVID19 positive for events or other areas where ", size, " individuals are in close contact [Assuming 3:1 ascertainment bias]"),
+ media = file.path("daily_risk_map_spain", current_time, paste0(current_time,"_", asc_bias, "_", size, ".png")))
